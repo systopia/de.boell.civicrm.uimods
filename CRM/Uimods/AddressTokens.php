9a976284be499091ae8ff286dfa0025fc62d4792
systopia <endres@systopia.de>
2017-12-15 14:34:39 +0100

    fixed HBS-6268

--- CRM/Uimods/AddressTokens.php
+++ CRM/Uimods/AddressTokens.php
@@ -199,29 +199,30 @@ class CRM_Uimods_AddressTokens {
     if ($load_master && !empty($contactId_2_masterAddressId)) {
       // step 1: load all master addresses
       $master_query = civicrm_api3('Address', 'get', array(
-        'id'      => array('IN' => array_values($contactId_2_masterAddressId)),
-        'return'  => 'id,contact_id',
-        'options' => array('limit' => 0),
+        'id'         => array('IN' => array_values($contactId_2_masterAddressId)),
+        'return'     => 'id,contact_id',
+        'sequential' => 0,
+        'options'    => array('limit' => 0),
         ));
       $contactId_2_masterContactId = array();
-      $masterAddressId_2_contactId = array_flip($contactId_2_masterAddressId);
-      foreach ($master_query['values'] as $master_address) {
-        $contact_id = $masterAddressId_2_contactId[$master_address['id']];
-        $contactId_2_masterContactId[$contact_id] = $master_address['contact_id'];
+      foreach ($contactId_2_masterAddressId as $contact_id => $master_id) {
+        if (isset($master_query['values'][$master_id]['contact_id'])) {
+          $contactId_2_masterContactId[$contact_id] = $master_query['values'][$master_id]['contact_id'];
+        }
       }
 
       // step 2: load all master contacts and set values in $contactId_2_address
-      $masterContactId_2_contactId = array_flip($contactId_2_masterContactId);
       $orgname_line_1 = CRM_Uimods_Config::getOrgnameField(1);
       $orgname_line_2 = CRM_Uimods_Config::getOrgnameField(2);
       $master_contactquery = civicrm_api3('Contact', 'get', array(
-        'id'      => array('IN' => array_values($contactId_2_masterContactId)),
-        'return'  => "display_name,{$orgname_line_1},{$orgname_line_2}",
-        'options' => array('limit' => 0),
+        'id'         => array('IN' => array_values($contactId_2_masterContactId)),
+        'sequential' => 0,
+        'return'     => "display_name,{$orgname_line_1},{$orgname_line_2}",
+        'options'    => array('limit' => 0),
         ));
-      foreach ($master_contactquery['values'] as $master_contact) {
-        $contact_id = $masterContactId_2_contactId[$master_contact['id']];
-        $contactId_2_address[$contact_id]['master'] = $master_contact['display_name'];
+      foreach ($contactId_2_masterContactId as $contact_id => $master_contact_id) {
+        $master_contact = $master_contactquery['values'][$master_contact_id];
+        $contactId_2_address[$contact_id]['master']   = $master_contact['display_name'];
         $contactId_2_address[$contact_id]['master_1'] = $master_contact[$orgname_line_1];
         $contactId_2_address[$contact_id]['master_2'] = $master_contact[$orgname_line_2];
       }
